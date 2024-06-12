58238b994fd05d56d5f8728c56913ec7adc80b0e
Nether <100975664+GitNether@users.noreply.github.com>
2024-06-12 15:47:57 +0200

    lower price back to 6

--- joker/pool_table.lua
+++ joker/pool_table.lua
@@ -12,7 +12,7 @@ SMODS.Joker {
     rarity = 3,
     pos = { x = 9, y = 0 },
     atlas = 'jokers_atlas',
-    cost = 10,
+    cost = 6,
     unlocked = true,
     discovered = true,
     blueprint_compat = true,
