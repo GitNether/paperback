bb441812f7a6a7f0f0185c2b4e0f8cea7ad6bb7b
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-06-14 17:33:43 -0500

    Added full functionality. Just need to prevent from showing up in shop

--- joker/ghost_cola.lua
+++ joker/ghost_cola.lua
@@ -14,11 +14,35 @@ SMODS.Joker {
     cost = 6,
     unlocked = true,
     discovered = true,
-    blueprint_compat = true,
-    eternal_compat = true,
+    blueprint_compat = false,
+    eternal_compat = false,
     soul_pos = nil,
     
     calculate = function(self, card, context)
-        
+        if context.selling_self then
+            -- Adds the negative tag
+            G.E_MANAGER:add_event(Event({
+                func = (function()
+                    add_tag(Tag('tag_negative'))
+                    return true
+                end)
+            }))
+
+            -- Creates the negative spectral card
+            G.E_MANAGER:add_event(Event({
+                trigger = 'before',
+                delay = 0.0,
+                func = (function()
+                        local card = create_card('Spectral',G.consumeables, nil, nil, nil, nil, nil, 'sea')
+                        local edition = {negative = true}
+                        card:set_edition(edition, true)
+                        card:add_to_deck()
+                        G.consumeables:emplace(card)
+                    return true
+                end
+            )}))
+        end
     end,
 }
+
+
