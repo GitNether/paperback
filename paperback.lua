b8e285db47339b162ba312548253e7574e832684
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-06-14 19:51:49 -0500

    Add flag to check if Diet Cola has been sold on current run

--- paperback.lua
+++ paperback.lua
@@ -37,3 +37,36 @@ for key, enabled in pairs(CONFIG) do
         sendDebugMessage("Paperback :: Loaded joker: " .. key)
     end
 end
+
+
+
+-- Code to allow Ghost Cola to show up
+-- in the shop When Diet Cola is sold
+--------------------------------------------------
+
+-- Creates the flag
+local BackApply_to_run_ref = Back.apply_to_run
+function Back.apply_to_run(arg_56_0)
+    BackApply_to_run_ref(arg_56_0)
+    G.GAME.pool_flags.diet_cola_sold = false
+    G.P_CENTERS['j_diet_cola']['yes_pool_flag'] = 'diet_cola_sold'
+    G.P_CENTERS['j_pape_ghost_cola']['no_pool_flag'] = 'diet_cola_sold'
+end
+
+-- Modifies the `diet_cola_sold` flag
+local calculate_joker_ref = Card.calculate_joker
+function Card:calculate_joker(context)
+    local ret = calculate_joker_ref(self, context)
+
+    if self.ability.set == "Joker" and not self.debuff then
+        if context.selling_self then
+            if self.ability.name == 'Diet Cola' then
+                G.GAME.pool_flags.diet_cola_sold = true
+                sendDebugMessage("Diet Cola Sold!")
+            end
+        end
+    end
+
+    return ret
+end
+--------------------------------------------------
