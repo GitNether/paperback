d39421ab75008536e1ecd1c83b13fad679e19c90
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-07-02 00:43:21 -0500

    Add JokerDisplay mod support to Pool Table

--- joker/pool_table.lua
+++ joker/pool_table.lua
@@ -55,3 +55,97 @@ SMODS.Joker {
         end
     end
 }
+
+
+-- JokerDisplay Mod Integration
+if SMODS.Mods["JokerDisplay"] then
+    local jd_def = JokerDisplay.Definitions
+    jd_def["j_pape_pool_table"] = {
+        line_1 = {
+            {
+                ref_table = "card.joker_display_values",
+                ref_value = "active_text"
+            },
+            {
+                ref_table = "card.joker_display_values",
+                ref_value = "count"
+            },
+            {
+                ref_table = "card.joker_display_values",
+                ref_value = "active_text_pool_table",
+                colour = G.C.SECONDARY_SET.Planet
+            }
+        },
+        calc_function = function(card)
+            local faces = false
+            local planet_card = ""
+            local hand = next(G.play.cards) and G.play.cards or G.hand.highlighted
+            local text, _, scoring_hand = JokerDisplay.evaluate_hand(hand)
+
+            -- Checks each card in the scoring hand for a face card
+            for i = 1, #scoring_hand do
+                if scoring_hand[i]:is_face() then
+                    faces = true
+                    break
+                end
+            end
+
+            -- Gets the name of the scoring hand to be used in the description (Need a better way to get this info)
+            if faces then
+                planet_card = "Planet"
+            elseif text == "High Card" then
+                planet_card = "Pluto"
+            elseif text == "Pair" then
+                planet_card = "Mercury"
+            elseif text == "Two Pair" then
+                planet_card = "Uranus"
+            elseif text == "Three of a Kind" then
+                planet_card = "Venus"
+            elseif text == "Straight" then
+                planet_card = "Saturn"
+            elseif text == "Flush" then
+                planet_card = "Jupiter"
+            elseif text == "Full House" then
+                planet_card = "Earth"
+            elseif text == "Four of a Kind" then
+                planet_card = "Mars"
+            elseif text == "Straight Flush" then
+                planet_card = "Neptune"
+            elseif text == "Five of a Kind" then
+                planet_card = "Planet X"
+            elseif text == "Flush House" then
+                planet_card = "Ceres"
+            elseif text == "Flush Five" then
+                planet_card = "Eris"
+            end
+
+            -- Determines if the Joker's ability is active
+            card.joker_display_values.active = G.GAME and G.GAME.current_round.hands_played == 0
+            card.joker_display_values.active_text = card.joker_display_values.active and "+" or "(Inactive)"
+
+            -- If Joker's ability is active, display ability effect
+            if card.joker_display_values.active then
+                card.joker_display_values.count = faces and 0 or 1
+                card.joker_display_values.active_text_pool_table = " " .. planet_card
+
+                -- If no cards selected, display no planets gained
+                if #scoring_hand == 0 then
+                    card.joker_display_values.count = 0
+                    card.joker_display_values.active_text_pool_table = " Planet"
+                end
+            -- Otherwise, if inactive, do not display count or gained planet card
+            else
+                card.joker_display_values.count = ""
+                card.joker_display_values.active_text_pool_table = ""
+            end
+        end,
+
+        -- Sets the color of the description to be inactive text when ability not active
+        style_function = function(card, line_1, line_2)
+            if line_1 then
+                line_1.children[1].config.colour = card.joker_display_values.active and G.C.UI.TEXT_LIGHT or G.C.UI.TEXT_INACTIVE
+            end
+            return false
+        end
+    }
+end
