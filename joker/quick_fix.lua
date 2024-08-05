642943a199b434f6683d2dd78dde761709892176
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-08-04 22:34:15 -0500

    Finish up the Joker

--- joker/quick_fix.lua
+++ joker/quick_fix.lua
@@ -1,5 +1,5 @@
 SMODS.Joker {
-    key = 'quick_fux',
+    key = 'quick_fix',
     loc_txt = {
         name = "Quick Fix",
         text = {
@@ -41,32 +41,64 @@ SMODS.Joker {
     end,
     
     calculate = function(self, card, context)
-        if context.end_of_round and not context.blueprint then
-            if pseudorandom("quick_fix") < G.GAME.probabilities.normal/card.ability.extra.odds then 
+        if context.end_of_round and not context.blueprint and not (context.individual or context.repetition) then
+            if pseudorandom("Quick Fix") < G.GAME.probabilities.normal/card.ability.extra.odds then
                 G.E_MANAGER:add_event(Event({
                     func = function()
                         play_sound('tarot1')
                         card.T.r = -0.2
-                        card:juice_up(0.3, 0.4)
+                        card:juice_up(0.3,0.4)
                         card.states.drag.is = true
                         card.children.center.pinch.x = true
                         G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                            func = function()
-                                    G.jokers:remove_card(card)
-                                    card:remove()
-                                    card = nil
-                                return true; end})) 
+                                                    func = function()
+                                                        G.jokers:remove_card(card)
+                                                        card:remove()
+                                                        card = nil
+                                                        return true; end }))
                         return true
                     end
                 }))
+
                 return {
-                    message = "Destroyed!"
+                    message = "Destroyed",
+                    colour = G.C.MULT,
+                    card = card
                 }
             else
                 return {
-                    message = localize('k_safe_ex')
+                    message = localize('k_safe_ex'),
+                    colour = G.C.MULT,
+                    card = card
                 }
             end
         end
     end
 }
+
+
+-- JokerDisplay integration
+if SMODS.Mods["JokerDisplay"] and _G["JokerDisplay"] then
+    JokerDisplay.Definitions["j_pape_quick_fix"] = {
+        reminder_text = {
+            { text = "(+" },
+            { ref_table = 'card.ability.extra', ref_value = 'h_size' },
+            { text = ")" },
+        },
+
+        reminder_text_config = {
+            scale = 0.35
+        },
+
+        extra = {
+            {
+                { text = "(" },
+                { ref_table = 'G.GAME.probabilities', ref_value = 'normal' },
+                { text = " in " },
+                { ref_table = 'card.ability.extra', ref_value = 'odds' },
+                { text = ')' },
+            }
+        },
+        extra_config = { colour = G.C.GREEN, scale = 0.3 }
+    }
+end
