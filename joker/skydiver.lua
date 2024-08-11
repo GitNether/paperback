01daafc24425a0b0843e6e05fc84562c947828e0
Daniel Hoelzeman <DHOELZEMAN@cub.uca.edu>
2024-08-11 00:56:00 -0500

    Fix Skydiver effect

--- joker/skydiver.lua
+++ joker/skydiver.lua
@@ -3,16 +3,16 @@ SMODS.Joker {
     loc_txt = {
         name = "Skydiver",
         text = {
-            "{C:white,X:mult}X#1#{} Mult if all {C:attention}scored cards{} are equal",
-            "to or lower than the {C:attention}lowest ranked{} card scored this round.",
-            "Lowest card updated at end of played hand.",
+            "{C:white,X:mult}X#1#{} Mult if all {C:attention}scored cards{}",
+            "are equal to or less than the {C:attention}lowest ranked{}",
+            "card scored this round. Lowest rank updated at",
+            "end of played {C:chips}hand.",
             "{C:inactive}(Currently: #2#)"
         }
     },
     config = {
         extra = {
-            Xmult = 1,
-            Xmult_mod = 3,
+            x_mult = 3,
             lowest_rank = 14,
         }
     },
@@ -27,29 +27,67 @@ SMODS.Joker {
     soul_pos = nil,
 
     loc_vars = function(self, info_queue, center)
-        center.ability.extra.lowest_rank = get_rank(center.ability.extra.lowest_rank)
+        local lowest_rank = get_rank(center.ability.extra.lowest_rank)
 
         return {
             vars = {
-                center.ability.extra.Xmult_mod,
-                center.ability.extra.lowest_rank
+                center.ability.extra.x_mult,
+                lowest_rank
             }
         }
     end,
 
     -- Calculate function for the Joker
     calculate = function(self, card, context)
-        if context.scoring_hand then
+        if not card.debuff then
+            -- Give the xMult during play if conditions are met
+            if context.joker_main then
+                local active = true
 
-        end
+                -- Loop through the scoring_hand 
+                for i=1, #context.scoring_hand do
+                    if context.scoring_hand[i]:get_id() > card.ability.extra.lowest_rank then
+                        active = false
+                        break
+                    end
+                end
+                if active then
+                    return {
+                        message = localize{type='variable',key='a_xmult',vars={card.ability.extra.x_mult}},
+                        Xmult_mod = card.ability.extra.x_mult,
+                        card = card
+                    }
+                end
+            end
 
-        if context.end_of_round and not context.blueprint and not context.individual and not context.repetition then
-            card.ability.extra.lowest_rank = 14
-            card_eval_status_text(card, 'extra', nil, nil, nil, { message = localize('k_reset'), colour = G.C.SECONDARY_SET.Planet})
-            -- return {
-            --     card = self,
-            --     message = localize('k_reset')
-            -- }
+            if context.after and not (context.individual or context.repetition) and not context.blueprint then
+                local last_lowest = card.ability.extra.lowest_rank
+
+                if context.scoring_hand then
+                    for _, v in pairs(context.scoring_hand) do
+                        if v:get_id() < card.ability.extra.lowest_rank then
+                            card.ability.extra.lowest_rank = v:get_id()
+                        end
+                    end
+
+                    if card.ability.extra.lowest_rank < last_lowest then
+                        return {
+                            message = tostring(card.ability.extra.lowest_rank),
+                            card = card
+                        }
+                    end
+                end
+            end
+
+            if context.end_of_round and not (context.individual or context.repetition) and G.GAME.blind.boss and not context.blueprint then
+                card.ability.extra.lowest_rank = 14
+
+                return {
+                    message = localize('k_reset'),
+                    colour = G.C.MULT,
+                    card = card
+                }
+            end
         end
     end
 }
@@ -65,6 +103,6 @@ function get_rank(rank)
     elseif rank == 14 then
         return "Ace"
     else
-        return rank
+        return tostring(rank)
     end
 end
