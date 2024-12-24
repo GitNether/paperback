fd8a8c51f1dd2a768abfd6b26e6724ef9dc8b563
OppositeWolf770 <dhoelzeman@proton.me>
2024-12-24 02:03:22 -0600

    Implement 4/5 panorama jokers

---
+++ joker/blue_bonnets.lua
@@ -0,0 +1,45 @@
+SMODS.Joker {
+  key = "blue_bonnets",
+  loc_txt = {
+    name = "Blue Bonnets",
+    text = {
+      "Scored {C:clubs}Clubs{} give {C:mult}+#1#{} Mult for",
+      "every {C:money}${} in {C:money}Sell Value{} this Joker has",
+      "{C:inactive}(Currently {C:mult}+#2#{} {C:inactive}Mult)"
+    }
+  },
+  config = {
+    extra = {
+      a_mult = 1
+    }
+  },
+  rarity = 1,
+  pos = { x = 1, y = 4 },
+  atlas = "jokers_atlas",
+  cost = 6,
+  unlocked = true,
+  discovered = true,
+  blueprint_compat = true,
+  eternal_compat = true,
+  soul_pos = nil,
+
+  loc_vars = function(self, info_queue, card)
+    return {
+      vars = {
+        card.ability.extra.a_mult,
+        card.ability.extra.a_mult * card.sell_cost
+      }
+    }
+  end,
+
+  calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play then
+      if context.other_card:is_suit("Clubs") then
+        return {
+          mult = card.ability.extra.a_mult * card.sell_cost,
+          card = card
+        }
+      end
+    end
+  end
+}
--- joker/cherry_blossoms.lua
+++ joker/cherry_blossoms.lua
@@ -3,20 +3,20 @@ SMODS.Joker {
     loc_txt = {
         name = "Cherry Blossoms",
         text = {
-          "Scored {C:Hearts}Hearts{} give {C:mult}+#1#{} Mult for",
-          "every {C:money}${} in {C:money}Sell Value{} this Joker has",
-          "{C:inactive}(Currently {C:mult}+#2#{} {C:inactive}Mult)"
+            "Scored {C:hearts}Hearts{} give {C:mult}+#1#{} Mult for",
+            "every {C:money}${} in {C:money}Sell Value{} this Joker has",
+            "{C:inactive}(Currently {C:mult}+#2#{} {C:inactive}Mult)"
         }
     },
     config = {
         extra = {
-          a_mult = 1
+            a_mult = 1
         }
     },
     rarity = 1,
     pos = { x = 2, y = 4 },
     atlas = "jokers_atlas",
-    cost = 5,
+    cost = 6,
     unlocked = true,
     discovered = true,
     blueprint_compat = true,
@@ -26,8 +26,8 @@ SMODS.Joker {
     loc_vars = function(self, info_queue, card)
         return {
             vars = {
-              card.ability.extra.a_mult,
-              card.ability.extra.a_mult * card.sell_cost
+                card.ability.extra.a_mult,
+                card.ability.extra.a_mult * card.sell_cost
             }
         }
     end,
@@ -35,12 +35,11 @@ SMODS.Joker {
     calculate = function(self, card, context)
         if context.individual and context.cardarea == G.play then
             if context.other_card:is_suit("Hearts") then
-               return {
+                return {
                     mult = card.ability.extra.a_mult * card.sell_cost,
                     card = card
-                } 
+                }
             end
         end
     end
 }
-
