de4a413715c305bb1fcdf17ffcf86d40ddf31300
Daniel Hoelzeman <dhoelzeman@cub.uca.edu>
2024-12-23 21:09:09 -0600

    Started implementing Cherry Blossom

---
+++ joker/cherry_blossoms.lua
@@ -0,0 +1,46 @@
+SMODS.Joker {
+    key = "cherry_blossoms",
+    loc_txt = {
+        name = "Cherry Blossoms",
+        text = {
+          "Scored {C:Hearts}Hearts{} give {C:mult}+#1#{} Mult for",
+          "every {C:money}${} in {C:money}Sell Value{} this Joker has",
+          "{C:inactive}(Currently {C:mult}+#2#{} {C:inactive}Mult)"
+        }
+    },
+    config = {
+        extra = {
+          a_mult = 1
+        }
+    },
+    rarity = 1,
+    pos = { x = 2, y = 4 },
+    atlas = "jokers_atlas",
+    cost = 5,
+    unlocked = true,
+    discovered = true,
+    blueprint_compat = true,
+    eternal_compat = true,
+    soul_pos = nil,
+
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+              card.ability.extra.a_mult,
+              card.ability.extra.a_mult * card.sell_cost
+            }
+        }
+    end,
+
+    calculate = function(self, card, context)
+        if context.individual and context.cardarea == G.play then
+            if context.other_card:is_suit("Hearts") then
+               return {
+                    mult = card.ability.extra.a_mult * card.sell_cost,
+                    card = card
+                } 
+            end
+        end
+    end
+}
+
