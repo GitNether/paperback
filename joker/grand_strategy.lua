f2d6275157b9d38777430617acbc8c1afa949429
OppositeWolf770 <dhoelzeman@proton.me>
2024-12-27 17:45:11 -0600

    Added basis for Grand Strategy. Just need to get it implemented now.

---
+++ joker/grand_strategy.lua
@@ -0,0 +1,11 @@
+SMODS.Joker {
+  key = 'grand_strategy',
+  loc_txt = {
+    name = "Grand Strategy",
+    text = {
+      "This Joker gives {X:mult,C:white}X#1#{} Mult if",
+      "{C:attention}#2#{} unique {C:attention}enahncements{}, {C:attention}editions{},",
+      "or {C:attention}seals{} are in your full deck",
+    }
+  }
+}
