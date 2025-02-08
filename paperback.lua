93b1bd85b2d8c91a87d330bb0c697c47c8cfc8c1
srockw <136873475+srockw@users.noreply.github.com>
2025-02-08 05:22:49 -0300

    Add spreadsheet Minor Arcana to list

--- paperback.lua
+++ paperback.lua
@@ -168,7 +168,62 @@ local ENABLED_JOKERS = {
 }
 
 local ENABLED_MINOR_ARCANA = {
-  "two_of_cups"
+  -- "ace_of_cups",
+  "two_of_cups",
+  -- "three_of_cups",
+  -- "four_of_cups",
+  -- "five_of_cups",
+  -- "six_of_cups",
+  -- "seven_of_cups",
+  -- "eight_of_cups",
+  -- "nine_of_cups",
+  -- "ten_of_cups",
+  -- "page_of_cups",
+  -- "knight_of_cups",
+  -- "queen_of_cups",
+  -- "king_of_cups",
+  -- "ace_of_wands",
+  -- "two_of_wands",
+  -- "three_of_wands",
+  -- "four_of_wands",
+  -- "five_of_wands",
+  -- "six_of_wands",
+  -- "seven_of_wands",
+  -- "eight_of_wands",
+  -- "nine_of_wands",
+  -- "ten_of_wands",
+  -- "page_of_wands",
+  -- "knight_of_wands",
+  -- "queen_of_wands",
+  -- "king_of_wands",
+  -- "ace_of_swords",
+  -- "two_of_swords",
+  -- "three_of_swords",
+  -- "four_of_swords",
+  -- "five_of_swords",
+  -- "six_of_swords",
+  -- "seven_of_swords",
+  -- "eight_of_swords",
+  -- "nine_of_swords",
+  -- "ten_of_swords",
+  -- "page_of_swords",
+  -- "knight_of_swords",
+  -- "queen_of_swords",
+  -- "king_of_swords",
+  -- "ace_of_pentacles",
+  -- "two_of_pentacles",
+  -- "three_of_pentacles",
+  -- "four_of_pentacles",
+  -- "five_of_pentacles",
+  -- "six_of_pentacles",
+  -- "seven_of_pentacles",
+  -- "eight_of_pentacles",
+  -- "nine_of_pentacles",
+  -- "ten_of_pentacles",
+  -- "page_of_pentacles",
+  -- "knight_of_pentacles",
+  -- "queen_of_pentacles",
+  -- "king_of_pentacles",
 }
 
 -- Only load jokers if they are enabled in the config
@@ -181,11 +236,12 @@ if PB_UTIL.config.minor_arcana_enabled then
   -- Register the consumable type to be used by Minor Arcana
   SMODS.ConsumableType {
     key = 'minor_arcana',
-    prefix_config = { key = true },     -- Add the prefix of the mod to the key
+    prefix_config = { key = true },      -- Add the prefix of the mod to the key
     primary_colour = HEX("BDA0D9"),
-    secondary_colour = HEX("BDA0D9"),   -- Color of the collection button and badge
-    shop_rate = 0,                      -- These will not appear in the shop
-    default = 'c_paperback_two_of_cups' -- Card to spawn if pool is empty
+    secondary_colour = HEX("BDA0D9"),    -- Color of the collection button and badge
+    shop_rate = 0,                       -- These will not appear in the shop
+    default = 'c_paperback_two_of_cups', -- Card to spawn if pool is empty
+    collection_rows = { 7, 7 }
   }
 
   PB_UTIL.register_items(ENABLED_MINOR_ARCANA, "minor_arcana")
