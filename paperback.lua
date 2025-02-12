ce795789256a4f1f929a59bbd69bca5468e84e7a
Daniel Hoelzeman <59400343+OppositeWolf770@users.noreply.github.com>
2025-02-11 23:10:50 -0600

    Create atlas file and move atlas definitions to it

--- paperback.lua
+++ paperback.lua
@@ -7,68 +7,8 @@ NFS.load(SMODS.current_mod.path .. "/utilities/ui.lua")()
 NFS.load(SMODS.current_mod.path .. "/utilities/hooks.lua")()
 NFS.load(SMODS.current_mod.path .. "/utilities/cross-mod.lua")()
 
--- Registers the atlas for Jokers
-SMODS.Atlas {
-  key = 'jokers_atlas',
-  px = 71,
-  py = 95,
-  path = 'Jokers.png'
-}
-
--- Registers the atlas for Minor Arcana
-SMODS.Atlas {
-  key = 'minor_arcana_atlas',
-  px = 71,
-  py = 95,
-  path = 'MinorArcana.png'
-}
-
--- Registers the atlas for Enhancements
-SMODS.Atlas {
-  key = 'enhancements_atlas',
-  px = 71,
-  py = 95,
-  path = 'Enhancements.png'
-}
-
---- Registers atlas for Crowns and Stars
--- Low contrast
-SMODS.Atlas {
-  key = 'suits_lc',
-  path = 'suits_lc.png',
-  px = 71,
-  py = 95
-}
-
-SMODS.Atlas {
-  key = 'suits_ui_lc',
-  path = 'suits_ui_lc.png',
-  px = 18,
-  py = 18
-}
-
--- High contrast
-SMODS.Atlas {
-  key = 'suits_hc',
-  path = 'suits_hc.png',
-  px = 71,
-  py = 95
-}
-
-SMODS.Atlas {
-  key = 'suits_ui_hc',
-  path = 'suits_ui_hc.png',
-  px = 18,
-  py = 18
-}
-
--- Registers the mod icon
-SMODS.Atlas { -- modicon
-  key = 'modicon',
-  px = 32,
-  py = 32,
-  path = 'modicon.png'
-}
+-- Load the atlases
+NFS.load(SMODS.current_mod.path .. "/content/atlas.lua")()
 
 -- Disable specific jokers by commenting them out
 local ENABLED_JOKERS = {
