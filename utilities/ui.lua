51e1e7668345a880f25e818e6934ed318c5c04dd
srockw <136873475+srockw@users.noreply.github.com>
2025-02-12 18:38:16 -0300

    Slight ui.lua refactoring

--- utilities/ui.lua
+++ utilities/ui.lua
@@ -2,26 +2,13 @@
 SMODS.current_mod.config_tab = function()
   return {
     n = G.UIT.ROOT,
-    config = {
-      align = 'cm',
-      padding = 0.05,
-      emboss = 0.05, -- raises this element
-      r = 0.1,       -- adds a radius to the borders
-      colour = G.C.BLACK
-    },
+    config = { align = 'cm', padding = 0.05, emboss = 0.05, r = 0.1, colour = G.C.BLACK },
     nodes = {
       {
-        n = G.UIT.R, -- Row
+        n = G.UIT.R,
         config = { align = 'cm', minh = 1 },
         nodes = {
-          {
-            n = G.UIT.T, -- Text
-            config = {
-              text = localize('paperback_ui_requires_restart'),
-              colour = G.C.RED,
-              scale = 0.5
-            }
-          }
+          { n = G.UIT.T, config = { text = localize('paperback_ui_requires_restart'), colour = G.C.RED, scale = 0.5 } }
         }
       },
       create_toggle {
@@ -45,182 +32,106 @@ end
 
 -- Create Credits tab in our mod UI
 SMODS.current_mod.extra_tabs = function()
-  return {
-    {
-      label = localize('b_credits'),
-      tab_definition_function = function()
-        return {
-          n = G.UIT.ROOT,
-          config = {
-            align = 'tl',
-            padding = 0.05,
-            emboss = 0.05, -- raises this element
-            r = 0.1,       -- adds a radius to the borders
-            colour = G.C.BLACK
-          },
+  local credits_tab = {
+    n = G.UIT.ROOT,
+    config = { align = 'tl', padding = 0.05, emboss = 0.05, r = 0.1, colour = G.C.BLACK },
+    nodes = { {
+      n = G.UIT.R,
+      nodes = {
+        {
+          n = G.UIT.C,
+          config = { padding = 0.5 },
           nodes = {
             {
-              n = G.UIT.R, -- Row
+              n = G.UIT.R,
               nodes = {
-                {
-                  n = G.UIT.C, -- Column
-                  config = { padding = 0.5 },
-                  nodes = {
-                    {
-                      n = G.UIT.R, -- Row
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = localize('paperback_ui_artists'),
-                            colour = G.C.CHIPS,
-                            scale = 0.75
-                          }
-                        },
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'PaperMoon',
-                            colour = G.C.MULT,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    },
-                  }
-                },
-                {
-                  n = G.UIT.C, -- Column
-                  config = { padding = 0.5 },
-                  nodes = {
-                    {
-                      n = G.UIT.R, -- Row
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = localize('paperback_ui_developers'),
-                            colour = G.C.CHIPS,
-                            scale = 0.75
-                          }
-                        },
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'OppositeWolf770',
-                            colour = G.C.GREEN,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'srockw',
-                            colour = G.C.GREEN,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'Nether',
-                            colour = G.C.GREEN,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'B',
-                            colour = G.C.GREEN,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    }
-                  }
-                },
-                {
-                  n = G.UIT.C, -- Column
-                  config = { padding = 0.5 },
-                  nodes = {
-                    {
-                      n = G.UIT.R, -- Row
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = localize('paperback_ui_localization'),
-                            colour = G.C.CHIPS,
-                            scale = 0.75
-                          }
-                        },
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'pinkmaggit-hub',
-                            colour = G.C.FILTER,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    },
-                    {
-                      n = G.UIT.R, -- Row
-                      config = { align = 'cm', minh = 0.25 },
-                      nodes = {
-                        {
-                          n = G.UIT.T, -- Text
-                          config = {
-                            text = 'mathieulievre',
-                            colour = G.C.FILTER,
-                            scale = 0.4
-                          }
-                        }
-                      }
-                    },
-                  }
-                },
+                { n = G.UIT.T, config = { text = localize('paperback_ui_artists'), colour = G.C.CHIPS, scale = 0.75 } },
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'PaperMoon', colour = G.C.MULT, scale = 0.4 } }
+              }
+            },
+          }
+        },
+        {
+          n = G.UIT.C,
+          config = { padding = 0.5 },
+          nodes = {
+            {
+              n = G.UIT.R,
+              nodes = {
+                { n = G.UIT.T, config = { text = localize('paperback_ui_developers'), colour = G.C.CHIPS, scale = 0.75 } },
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'OppositeWolf770', colour = G.C.GREEN, scale = 0.4 } }
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'srockw', colour = G.C.GREEN, scale = 0.4 } }
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'Nether', colour = G.C.GREEN, scale = 0.4 } }
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'B', colour = G.C.GREEN, scale = 0.4 } }
+              }
+            }
+          }
+        },
+        {
+          n = G.UIT.C,
+          config = { padding = 0.5 },
+          nodes = {
+            {
+              n = G.UIT.R,
+              nodes = {
+                { n = G.UIT.T, config = { text = localize('paperback_ui_localization'), colour = G.C.CHIPS, scale = 0.75 } },
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'pinkmaggit-hub', colour = G.C.FILTER, scale = 0.4 } }
+              }
+            },
+            {
+              n = G.UIT.R,
+              config = { align = 'cm', minh = 0.25 },
+              nodes = {
+                { n = G.UIT.T, config = { text = 'mathieulievre', colour = G.C.FILTER, scale = 0.4 } }
               }
             }
           }
         }
+      }
+    } }
+  }
+
+  return {
+    {
+      label = localize('b_credits'),
+      tab_definition_function = function()
+        return credits_tab
       end
     }
   }
