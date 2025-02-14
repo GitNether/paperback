06ff327ada4cf4b4c3e72881a24ea2a84998c73c
OppositeWolf770 <dhoelzeman@proton.me>
2025-02-13 23:13:33 -0600

    (Autumn Leaves) Began work on JokerDisplay Definition

--- content/joker/autumn_leaves.lua
+++ content/joker/autumn_leaves.lua
@@ -2,6 +2,7 @@ SMODS.Joker {
   key = "autumn_leaves",
   config = {
     extra = {
+      suit = "Diamonds",
       xMult = 1.05,
       xMult_gain = 0.05,
       xMult_base = 1.05
@@ -20,22 +21,24 @@ SMODS.Joker {
   loc_vars = function(self, info_queue, card)
     return {
       vars = {
+        localize(card.ability.extra.suit, "suits_plural"),
         card.ability.extra.xMult_base,
-        card.ability.extra.xMult_gain
+        card.ability.extra.xMult_gain,
+        localize(card.ability.extra.suit, "suits_singular")
       }
     }
   end,
 
   calculate = function(self, card, context)
     if context.individual and context.cardarea == G.play then
-      -- Reset the xMult if the current card is not a diamond
-      if not context.other_card:is_suit("Diamonds") then
+      -- Reset the xMult if the current card is not the required suit
+      if not context.other_card:is_suit(card.ability.extra.suit) then
         card.ability.extra.xMult = card.ability.extra.xMult_base
         return
       end
 
-      -- Give the xMult if the current card is a diamond
-      if context.other_card:is_suit("Diamonds") then
+      -- Give the xMult if the current card is the required suit
+      if context.other_card:is_suit(card.ability.extra.suit) then
         -- Upgrade the xMult
         local xMult = card.ability.extra.xMult
         card.ability.extra.xMult = card.ability.extra.xMult + card.ability.extra.xMult_gain
@@ -51,5 +54,31 @@ SMODS.Joker {
     if context.after and not context.blueprint then
       card.ability.extra.xMult = card.ability.extra.xMult_base
     end
-  end
+  end,
+
+  -- joker_display_def = function(JokerDisplay)
+  --   return {
+  --     text = {
+  --       {
+  --         border_nodes = {
+  --           { text = "X" },
+  --           { ref_table = "card.joker_display_values", ref_value = "x_mult", retrigger_type = "exp" },
+  --         }
+  --       }
+  --     },
+
+  --     calc_function = function(card)
+  --       local playing_hand = next(G.play.cards)
+  --       local count = 0
+  --       for _, playing_card in ipairs(G.hand.cards) do
+  --         if playing_hand or playing_card.highlighted then
+  --           if playing_card:is_suit(card.ability.extra.suit) and not playing_card.debuff then
+  --             count = count + JokerDisplay.calculate_card_triggers(playing_card, nil, false)
+  --           end
+  --         end
+  --       end
+  --       card.joker_display_values.x_mult = card.ability.extra.
+  --     end
+  --   }
+  -- end
 }
