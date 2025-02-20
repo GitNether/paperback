3371f90214d2ab8a80f25fce6b5c336badf77f5d
VictinZero <79382302+VictinZero@users.noreply.github.com>
2025-02-21 00:50:41 +0100

    Add Balatrochrome shader

---
+++ assets/shaders/balatrochrome.fs
@@ -0,0 +1,144 @@
+#if defined(VERTEX) || __VERSION__ > 100 || defined(GL_FRAGMENT_PRECISION_HIGH)
+    #define PRECISION highp
+#else
+    #define PRECISION mediump
+#endif
+
+// !! change this variable name to your Shader's name
+// YOU MUST USE THIS VARIABLE IN THE vec4 effect AT LEAST ONCE
+
+// Values of this variable:
+// self.ARGS.send_to_shader[1] = math.min(self.VT.r*3, 1) + (math.sin(G.TIMERS.REAL/28) + 1) + (self.juice and self.juice.r*20 or 0) + self.tilt_var.amt
+// self.ARGS.send_to_shader[2] = G.TIMERS.REAL
+extern PRECISION vec2 balatrochrome;
+
+extern PRECISION number dissolve;
+extern PRECISION number time;
+// [Note] sprite_pos_x _y is not a pixel position!
+//        To get pixel position, you need to multiply  
+//        it by sprite_width _height (look flipped.fs)
+// (sprite_pos_x, sprite_pos_y, sprite_width, sprite_height) [not normalized]
+extern PRECISION vec4 texture_details;
+// (width, height) for atlas texture [not normalized]
+extern PRECISION vec2 image_details;
+extern bool shadow;
+extern PRECISION vec4 burn_colour_1;
+extern PRECISION vec4 burn_colour_2;
+
+// NEW
+
+// [Required] 
+// Apply dissolve effect (when card is being "burnt", e.g. when consumable is used)
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv);
+
+// This is what actually changes the look of card
+vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords )
+{
+    // Take pixel color (rgba) from `texture` at `texture_coords`, equivalent of texture2D in GLSL
+    vec4 tex = Texel(texture, texture_coords);
+    // Position of a pixel within the sprite
+	vec2 uv = (((texture_coords)*(image_details)) - texture_details.xy*texture_details.ba)/texture_details.ba;
+
+    const vec3 BALATRO_RED = vec3(0.992, 0.373, 0.333);
+    const vec3 BALATRO_BLUE = vec3(0., 0.612, 0.992);
+
+    // Invert the colors to preserve pure white
+    vec3 anti_balatro_red = vec3(1.) - BALATRO_RED;
+    vec3 anti_balatro_blue = vec3(1.) - BALATRO_BLUE;
+    vec3 anti_tex = vec3(1.) - tex.rgb;
+
+    // OLD: Compute the normal to the plane defined by two colors,
+    // then compute the projection onto that plane.
+    // vec3 normal = cross(anti_balatro_red, anti_balatro_blue);
+    // vec3 normal_proj = dot(anti_tex, normal) * normal / dot(normal, normal);
+    // vec3 plane_proj = anti_tex - normal_proj;
+
+    // OLD: Invert to return to original space, then clamp to [0, 1].
+    // tex.rgb = clamp(vec3(1.) - plane_proj, 0., 1.);
+
+    // Project the inverted original colors onto the inverted Balatro colors
+    // BUT swap the Balatro red and Balatro blue components
+    // (think of it like swapping the red and blue channels)
+    vec3 swapped_red_proj = dot(anti_tex, anti_balatro_red) * anti_balatro_blue / dot(anti_balatro_blue, anti_balatro_blue);
+    vec3 swapped_blue_proj = dot(anti_tex, anti_balatro_blue) * anti_balatro_red / dot(anti_balatro_red, anti_balatro_red);
+
+    // Three steps in one line:
+    // 1. Combine the projected colors to obtain the color in a 2D space;
+    // 2. Invert the result to go back to the original, non-inverted space;
+    // 3. Clamp the result to [0, 1].
+    tex.rgb = clamp(vec3(1.) - swapped_red_proj - swapped_blue_proj, 0., 1.);
+
+    // Maybe the clamp should have different, color-wise bounds,
+    // but with the component swap, it's not clear what the bounds should be
+
+    // OLD: Swap red and blue channels
+    // float tmp = tex.r;
+    // tex.r = tex.b;
+    // tex.b = tmp;
+
+    // Does not do anything. Required for shader to not crash.
+    if (uv.x > 2. * uv.x) {
+        uv = balatrochrome;
+    }
+
+    // required
+    return dissolve_mask(tex, texture_coords, uv);
+}
+
+vec4 dissolve_mask(vec4 tex, vec2 texture_coords, vec2 uv)
+{
+    if (dissolve < 0.001) {
+        return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, shadow ? tex.a*0.3: tex.a);
+    }
+
+    float adjusted_dissolve = (dissolve*dissolve*(3.-2.*dissolve))*1.02 - 0.01; //Adjusting 0.0-1.0 to fall to -0.1 - 1.1 scale so the mask does not pause at extreme values
+
+	float t = time * 10.0 + 2003.;
+	vec2 floored_uv = (floor((uv*texture_details.ba)))/max(texture_details.b, texture_details.a);
+    vec2 uv_scaled_centered = (floored_uv - 0.5) * 2.3 * max(texture_details.b, texture_details.a);
+	
+	vec2 field_part1 = uv_scaled_centered + 50.*vec2(sin(-t / 143.6340), cos(-t / 99.4324));
+	vec2 field_part2 = uv_scaled_centered + 50.*vec2(cos( t / 53.1532),  cos( t / 61.4532));
+	vec2 field_part3 = uv_scaled_centered + 50.*vec2(sin(-t / 87.53218), sin(-t / 49.0000));
+
+    float field = (1.+ (
+        cos(length(field_part1) / 19.483) + sin(length(field_part2) / 33.155) * cos(field_part2.y / 15.73) +
+        cos(length(field_part3) / 27.193) * sin(field_part3.x / 21.92) ))/2.;
+    vec2 borders = vec2(0.2, 0.8);
+
+    float res = (.5 + .5* cos( (adjusted_dissolve) / 82.612 + ( field + -.5 ) *3.14))
+    - (floored_uv.x > borders.y ? (floored_uv.x - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y > borders.y ? (floored_uv.y - borders.y)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.x < borders.x ? (borders.x - floored_uv.x)*(5. + 5.*dissolve) : 0.)*(dissolve)
+    - (floored_uv.y < borders.x ? (borders.x - floored_uv.y)*(5. + 5.*dissolve) : 0.)*(dissolve);
+
+    if (tex.a > 0.01 && burn_colour_1.a > 0.01 && !shadow && res < adjusted_dissolve + 0.8*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+        if (!shadow && res < adjusted_dissolve + 0.5*(0.5-abs(adjusted_dissolve-0.5)) && res > adjusted_dissolve) {
+            tex.rgba = burn_colour_1.rgba;
+        } else if (burn_colour_2.a > 0.01) {
+            tex.rgba = burn_colour_2.rgba;
+        }
+    }
+
+    return vec4(shadow ? vec3(0.,0.,0.) : tex.xyz, res > adjusted_dissolve ? (shadow ? tex.a*0.3: tex.a) : .0);
+}
+
+// for transforming the card while your mouse is on it
+extern PRECISION vec2 mouse_screen_pos;
+extern PRECISION float hovering;
+extern PRECISION float screen_scale;
+
+#ifdef VERTEX
+vec4 position( mat4 transform_projection, vec4 vertex_position )
+{
+    if (hovering <= 0.){
+        return transform_projection * vertex_position;
+    }
+    float mid_dist = length(vertex_position.xy - 0.5*love_ScreenSize.xy)/length(love_ScreenSize.xy);
+    vec2 mouse_offset = (vertex_position.xy - mouse_screen_pos.xy)/screen_scale;
+    float scale = 0.2*(-0.03 - 0.3*max(0., 0.3-mid_dist))
+                *hovering*(length(mouse_offset)*length(mouse_offset))/(2. -mid_dist);
+
+    return transform_projection * vertex_position + vec4(0,0,0,scale);
+}
+#endif
