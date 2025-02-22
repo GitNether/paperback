6b144ebc8f7beeeed0343aace1dcaacf4dce90b1
srockw <136873475+srockw@users.noreply.github.com>
2025-02-22 16:01:40 -0300

    (The World) Now affects "The House" boss blind

--- content/joker/the_world.lua
+++ content/joker/the_world.lua
@@ -11,10 +11,8 @@ SMODS.Joker {
   soul_pos = nil,
 }
 
-local calculate_joker_ref = Card.calculate_joker
-function Card.calculate_joker(self, context)
+local function wrap_the_world(ignores, func)
   local joker = next(SMODS.find_card('j_paperback_the_world'))
-  local ignores = (self.config.center.paperback or {}).ignores_the_world
 
   local previous = {
     hands_played = G.GAME.current_round.hands_played,
@@ -28,7 +26,7 @@ function Card.calculate_joker(self, context)
     end
   end
 
-  local ret = calculate_joker_ref(self, context)
+  local ret, ret2 = func()
 
   if joker and not ignores then
     for k, v in pairs(previous) do
@@ -36,5 +34,23 @@ function Card.calculate_joker(self, context)
     end
   end
 
-  return ret
+  return ret, ret2
+end
+
+-- Joker calculations
+local calculate_joker_ref = Card.calculate_joker
+function Card.calculate_joker(self, context)
+  local ignores = (self.config.center.paperback or {}).ignores_the_world
+
+  return wrap_the_world(ignores, function()
+    return calculate_joker_ref(self, context)
+  end)
+end
+
+-- Just handles "The House" calculations for now
+local stay_flipped_ref = Blind.stay_flipped
+function Blind.stay_flipped(self, area, card)
+  return wrap_the_world(nil, function()
+    return stay_flipped_ref(self, area, card)
+  end)
 end
