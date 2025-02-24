ec478abeba8e2a62b13e96f45db376f6ec90f06d
OppositeWolf770 <dhoelzeman@proton.me>
2025-02-23 22:51:08 -0600

    (Summoning Circle) Fix bug with Incantation mod installed

--- content/joker/summoning_circle.lua
+++ content/joker/summoning_circle.lua
@@ -19,8 +19,31 @@ SMODS.Joker {
             -- Add the card to G.consumeables
             G.E_MANAGER:add_event(Event({
               func = function()
-                local copied_card = pseudorandom_element(G.consumeables.cards, pseudoseed('summoning_circle'))
-                copied_card = copy_card(copied_card, nil, nil, nil, copied_card.edition and copied_card.edition.negative)
+                local copied_card
+                -- Incantation support
+                if next(SMODS.find_mod('incantation')) then
+                  local total, checked = 0, 0
+                  for i = 1, #G.consumeables.cards do
+                    total = total + (G.consumeables.cards[i]:getQty())
+                  end
+                  local poll = pseudorandom(pseudoseed('summoning_circle')) * total
+                  for i = 1, #G.consumeables.cards do
+                    checked = checked + (G.consumeables.cards[i]:getQty())
+                    if checked >= poll then
+                      copied_card = G.consumeables.cards[i]
+                      break
+                    end
+                  end
+                else
+                  copied_card = pseudorandom_element(G.consumeables.cards, pseudoseed('summoning_circle'))
+                end
+                copied_card = copy_card(copied_card, nil, nil, nil,
+                  copied_card.edition and copied_card.edition.negative)
+                -- More Incantation support
+                if next(SMODS.find_mod('incantation')) then
+                  copied_card.ability.qty = 1
+                  copied_card:set_cost()
+                end
                 copied_card:add_to_deck()
                 G.consumeables:emplace(copied_card)
                 return true
